7f3e88b57dd959a8bdf4ef29f6670dfc111425c2
Aamir <hiren@pop-os.localdomain>
2022-06-08 12:46:30 +0530

    change env variable

--- src/utils/getImage.js
+++ src/utils/getImage.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getCoverImage = async () => {
   try {
     return await axios.get(
-      `https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_UNSPLASH_KEY}`,
+      `https://api.unsplash.com/photos/random/?client_id=nC6cZ2w0pVKQ1Xu93LvOmaZszLlpjhYoJ-lbBBdHOpA`,
       {
         params: { query: { category: "landscapes" }, orientation: "landscape" },
       }
